e74f02f102c6b7f9a6fc01f265e61a0d017adadd
ayan shukla <ayanshukla4@gmail.com>
2020-12-07 02:49:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const translateBtn = document.querySelector('#click');
 const textInput = document.querySelector('#text-input');
 const output = document.querySelector('#output');
 
-translateBtn.addEventListener('click', () =>{
-    output.innerText = textInput.value;
-})
+const url = 'https://api.funtranslations.com/translate/minion.json';
 
-const url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+const getTranslationURL = (text) =>{
+    return url + '?' + 'text=' + text
+}
 
-fetch(url).then(response => response.json()).then(data => console.log(data))
+const errorHandler = (error) =>{
+    alert(`something went wrong : ${error}`)
+}
+
+    translateBtn.addEventListener('click', () =>{
+        const inputText = textInput.value
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(data => {
+            const translatedText = data.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler);
+    })
